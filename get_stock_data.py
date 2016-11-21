b168fd99438a0d53f922a7e9259ce94fe6200ced
Brad Sherman <bsherman1096@gmail.com>
2016-11-21 10:39:50 -0500

    initialized datascience project repository

---
+++ get_stock_data.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib
+import os
+
+base_url = "http://ichart.finance.yahoo.com/table.csv?s="
+
+
+def touch(path):
+    with open(path, 'w'):
+        os.utime(path, None)
+
+
+def make_url(ticker_symbol):
+    return base_url + ticker_symbol
+
+
+output_path = "."
+
+
+def make_filename(ticker_symbol, directory="stock_data"):
+    return output_path + "/" + directory + "/" + ticker_symbol + ".csv"
+
+
+def pull_historical_data(ticker_symbol, directory="stock_data"):
+
+    outfilename = output_path + \
+        "/" + "stock_data/" + ticker_symbol + ".csv"
+    touch(outfilename)
+
+    try:
+        urllib.urlretrieve(
+                make_url(ticker_symbol),
+                make_filename(ticker_symbol, directory))
+
+    except urllib.ContentTooShortError as e:
+        outfile = open(outfilename, "w")
+        outfile.write(e.content)
+        outfile.close()
+
+
+if __name__ == "__main__":
+    tickers = [
+            "WMT",
+            "FB",
+            "CMG",
+            "UAL",
+            "NKE",
+            "BAC",
+            "ADR",
+            "BZ",
+            "SOYB",
+            "SGG",
+            "UNG",
+            "JO",
+            "USO"]
+    for t in tickers:
+        print "Getting data for " + t
+        pull_historical_data(t)
